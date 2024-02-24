b1111bf71d29348f42b92f5f48ec835dea916377
k458 <58297249+k458@users.noreply.github.com>
2024-02-25 03:35:25 +0900

    C# some changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] strings = new string[5];
+int wordsAdded = 0;
+
+void PrintStringArray(string[] strings)
+{
+    Console.Write("[");
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (strings[i].Length >= 4) continue;
+        Console.Write('"' + strings[i] + '"');
+        if (i < strings.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+while (true)
+{
+    if (wordsAdded >= 5) break;
+    Console.Write($"Введите слово({1 + wordsAdded} из 5): ");
+    string input = Console.ReadLine();
+    if (input == "start" && wordsAdded > 0) break;
+    strings[wordsAdded] = input;
+    wordsAdded++;
+}
+Console.WriteLine("Results: ");
+
